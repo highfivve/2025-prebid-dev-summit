b8ac45c610c47ef200475622242a8848c524d40d
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2025-09-12 10:52:31 +0200

    Add gvlid alias and bidder params

--- mukiBidAdapter.ts
+++ mukiBidAdapter.ts
@@ -1,14 +1,23 @@
-import { ortbConverter } from '../libraries/ortbConverter/converter.js';
-import { BidderSpec, registerBidder } from '../src/adapters/bidderFactory.js';
-import { BANNER } from '../src/mediaTypes.js';
+import { AdapterRequest, BidderSpec, registerBidder } from '../src/adapters/bidderFactory.ts';
+import { BANNER } from '../src/mediaTypes.ts';
+import { ortbConverter } from '../libraries/ortbConverter/converter.ts';
 
 const BIDDER_CODE = 'muki';
-const ENDPOINT_URL = 'https://frag-muki.de/ortb2/auction.php';
+const ENDPOINT = 'https://frag-muki.de/ortb2/auction.php';
 const GVLID = 91;
 
-// ORTB standard for the win!
-const converter = ortbConverter({
-  context: { netRevenue: true, ttl: 20, },
+type MukiBidParams = {
+  test?: boolean
+}
+
+declare module '../src/adUnits' {
+  interface BidderParams {
+    [BIDDER_CODE]: MukiBidParams;
+  }
+}
+
+const converter = ortbConverter<typeof BIDDER_CODE>({
+  context: { netRevenue: true, ttl: 300, currency: 'EUR', mediaType: 'banner' },
   imp(buildImp, bidRequest, context) {
     return buildImp(bidRequest, context);
   },
@@ -26,25 +35,22 @@ export const spec: BidderSpec<typeof BIDDER_CODE> = {
   supportedMediaTypes: [BANNER],
   aliases: [
     // This is the dirty hack to hijack another bidder in the setup of a publisher. Don't do this at home!
-    { code: 'criteo' }
+    { code: 'criteo', gvlid: GVLID }
   ],
-
   isBidRequestValid: (_bid): boolean => {
     return true;
   },
-
-  buildRequests: (validBidRequests, bidderRequest) => {
+  buildRequests(validBidRequests, bidderRequest): AdapterRequest | AdapterRequest[] {
     if (validBidRequests && validBidRequests.length === 0) return [];
     const request = converter.toORTB({ bidRequests: validBidRequests, bidderRequest });
-    return { method: 'POST', url: ENDPOINT_URL, data: request };
+    return { method: 'POST', url: ENDPOINT, data: request };
   },
-
-  interpretResponse: (response, request) => {
-    if (response?.body) {
+  interpretResponse(response: { body: any; }, request) {
+    if (response.body) {
       return converter.fromORTB({ response: response.body, request: request.data });
     }
     return [];
   },
-};
+}
 
 registerBidder(spec);
